97057714171229a6cd76748caf068c899a41a724
jabe1204 <tjabet12@gmail.com>
2019-10-30 21:13:26 +0000

    Cree la funcion pitagoras que devuelve 1 si es posible un triangulo rectangulo y 0 si no

--- 2.cpp
+++ 2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-bool pitagoras(int x, int y, int z);
+int pitagoras(int x, int y, int z);
 
 int main(void)
 {
@@ -14,6 +14,33 @@ int main(void)
     std::cout<<"Ingrese el tercer valor"<<std::endl;
     int v3;
     std::cin>>v3;
-    
+    if(pitagoras(v1,v2,v3)==1)
+    {
+        std::cout<<"Con los valores dados SI se puede representar un triangulo rectangulo"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Con los valores dados NO se puede representar un triangulo rectangulo"<<std::endl;
+    }
     return 0;
+}
+
+int pitagoras(int x, int y, int z)
+{
+    if(x*x + y*y == z*z)
+    {
+        return 1;
+    }
+    else if(x*x + z*z == y*y)
+    {
+        return 1;
+    }
+    else if(y*y + z*z == x*x)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
