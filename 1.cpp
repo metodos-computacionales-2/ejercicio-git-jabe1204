bb25f44518ffb09660c27d01aff3b174616c5b95
jabe1204 <tjabet12@gmail.com>
2019-10-30 20:34:19 +0000

    Agrego la funcion area

--- 1.cpp
+++ 1.cpp
@@ -10,7 +10,7 @@ int main(void)
     std::cout<<"Ingrese el valor del radio"<<std::endl;
     int N;
     std::cin>>N;
-    std::cout<<"Con un radio de "<<N<<"\n"<<"El diametro es: "<<diametro(N)<<"\n"<<"El perimetro es: "<<perimetro(N)<<std::endl;
+    std::cout<<"Con un radio de "<<N<<"\n"<<"El diametro es: "<<diametro(N)<<"\n"<<"El perimetro es: "<<perimetro(N)<<"\n"<<"El area es: "<<area(N)<<std::endl;
     return 0;
 }
 
@@ -21,4 +21,8 @@ double diametro(int x)
 double perimetro(int x)
 {
  return 2*M_PI*x;
+}
+double area(int x)
+{
+ return M_PI*x*x;
 }
