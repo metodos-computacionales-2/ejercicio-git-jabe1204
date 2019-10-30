3159a1743af7f3f3d421746ea96674934df10d8c
jabe1204 <tjabet12@gmail.com>
2019-10-30 20:21:23 +0000

    función diametro

--- 1.cpp
+++ 1.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 
-double f(int x);
+double diametro(int x);
+double perimetro(int x);
+double area(int x);
 
 int main(void)
 {
     std::cout<<"Ingrese el valor del radio"<<std::endl;
     int N;
     std::cin>>N;
-    for(int i = 0; i<N;i++)
-    {
-      if(i%2 == 0)
-      {
-                std::cout<<f(i)<<"\t"<<i<<std::endl;
-      }
-    }
+    std::cout<<"Con un radio de "<<N<<" el diametro es "<<diametro(N)<<"\t"<<std::endl;
     return 0;
 }
 
-double f(int x)
+double diametro(int x)
 {
- return x*x;  
+ return 2*x;  
 }
     
