68eb6949d5d29b349aa44a8bbbaf8c5bbceaa737
jabe1204 <tjabet12@gmail.com>
2019-10-30 20:29:33 +0000

    Agrego la función perimetro y edito el cout para que salgan uno por linea

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 double diametro(int x);
 double perimetro(int x);
@@ -9,7 +10,7 @@ int main(void)
     std::cout<<"Ingrese el valor del radio"<<std::endl;
     int N;
     std::cin>>N;
-    std::cout<<"Con un radio de "<<N<<" el diametro es "<<diametro(N)<<"\t"<<std::endl;
+    std::cout<<"Con un radio de "<<N<<"\n"<<"El diametro es: "<<diametro(N)<<"\n"<<"El perimetro es: "<<perimetro(N)<<std::endl;
     return 0;
 }
 
@@ -17,4 +18,7 @@ double diametro(int x)
 {
  return 2*x;  
 }
-    
+double perimetro(int x)
+{
+ return 2*M_PI*x;
+}
