659986f2fc1a37800334a115d258c242e3c36f22
jabe1204 <tjabet12@gmail.com>
2019-10-30 20:46:57 +0000

    Agrego en el main el pedir los 3 valores

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+bool pitagoras(int x, int y, int z);
+
+int main(void)
+{
+    std::cout<<"Ingrese el primer valor"<<std::endl;
+    int v1;
+    std::cin>>v1;
+    std::cout<<"Ingrese el segundo valor"<<std::endl;
+    int v2;
+    std::cin>>v2;
+    std::cout<<"Ingrese el tercer valor"<<std::endl;
+    int v3;
+    std::cin>>v3;
+    
+    return 0;
+}
